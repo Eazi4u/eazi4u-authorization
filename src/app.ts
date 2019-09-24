552a739312ad5e966d7d5277e8ccd9b2ae634115
Michael <mihendricks1@gmail.com>
2019-09-24 11:47:07 +0200

    feat: Connected to mongodb 1. Create a mongo.connect file to connect to mongodb

--- src/app.ts
+++ src/app.ts
@@ -2,26 +2,32 @@ import colors from "colors";
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
+import objectPath from "object-path";
 
+import mongoConnect from "./infrasturcture/db/mongodb/mongo.connect";
 import authMiddlewareRouter from "./presentation/middleware/auth-routes.middleware";
 
 // Set configuration
 dotenv.config();
 colors.setTheme({});
 
-// import this after dotenv was set
+// Import config: should do this after dotenv was set env variables
 import config from "./config";
 console.log("config: ".yellow, colors.yellow(config));
 
+// Connect to MongoDB with mongoose
+mongoConnect();
+
+// Express server
 const app = express();
+app.listen(config.node_port, () => {
+  console.log(`Successfully running ${config.app_name} lisiting on port ${config.node_port}`.green);
+});
 
 // Middleware
 app.use(authMiddlewareRouter);
 
+// Routes
 app.get("/hello", (req: any, res: any) => {
   res.send("Hello World!");
-});
-
-app.listen(config.node_port, () => {
-  console.log(`Successfully connected to ${config.app_name} lisiting on port ${config.node_port}`.green);
 });
