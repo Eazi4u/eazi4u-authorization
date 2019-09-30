5df90225bc4f0247400b006df56abb810a804c00
Michael <mihendricks1@gmail.com>
2019-09-30 18:17:47 +0200

    feat: Register User 1. Adding User model to business logic.

--- src/application/logic/auth/auth.logic.ts
+++ src/application/logic/auth/auth.logic.ts
@@ -1,10 +1,26 @@
+import { IUser, User } from "../../../domain/aggregates/user/user.root-entity";
+
 export class AuthLogic {
   public login = async (): Promise<any> => {
     return "login logic";
   }
 
   public register = async (): Promise<any> => {
-    return "register logic";
+    try {
+      const user: IUser = new User({
+        username: "mike",
+        verified: false,
+        verifiedDate: new Date(),
+        roles: [],
+        createdAt: new Date(),
+        createdBy: "name"
+      });
+
+      await user.save();
+      return "register logic";
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   public logout = async (): Promise<any> => {
